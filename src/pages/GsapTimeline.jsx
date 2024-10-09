debfd921d70e528605d0e422dfddb2087a7db1b2
Antonio Felix <afelixjdev@gmail.com>
2024-10-09 14:55:30 -0400

    finished gsap beinngers training

--- src/pages/GsapTimeline.jsx
+++ src/pages/GsapTimeline.jsx
@@ -11,25 +11,31 @@ const GsapTimeline = () => {
   });
 
   useGSAP(() => {
-    timeline.to('#yellow-box',
-    {
-      x:250,
+    timeline.to("#yellow-box", {
+      x: 250,
       rotation: 360,
-      borderRadius: '100%',
-      duration:2,
-      ease: 'back.inOut'
-    } )
+      borderRadius: "100%",
+      duration: 2,
+      ease: "back.inOut",
+    });
 
-    timeline.to('#yellow-box',
-      {
-        x:500,
-        scale:1, 
-        rotation: 360,
-        borderRadius: '100%',
-        duration:2,
-        ease: 'back.inOut'
-      } )
+    timeline.to("#yellow-box", {
+      y: 250,
+      scale: 2,
+      rotation: 360,
+      borderRadius: "100%",
+      duration: 2,
+      ease: "back.inOut",
+    });
 
+    timeline.to("#yellow-box", {
+      x: 500,
+      scale: 1,
+      rotation: 360,
+      borderRadius: "100%",
+      duration: 2,
+      ease: "back.inOut",
+    });
   }, []);
 
   return (
@@ -66,7 +72,15 @@ const GsapTimeline = () => {
       </p>
 
       <div className="mt-20 space-y-10">
-        <button onClick={() => {}}>Play/Pause</button>
+        <button onClick={() => {
+          if(timeline.paused()){
+            timeline.play()
+          }
+          else{
+            timeline.pause();
+
+          }
+        }}>Play/Pause</button>
 
         <div id="yellow-box" className="w-20 h-20 bg-yellow-500 rounded-lg" />
       </div>
